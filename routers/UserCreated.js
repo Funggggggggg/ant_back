e64dd058ab1af5dd0f5c03802a07fdeaaa8866f2
fung <fung.8021@gmail.com>
2025-02-26 13:18:00 +0800

    2/26 上傳

--- routers/UserCreated.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { create } from '../controllers/user.js'
-
-const routerUsercreated = Router()
-
-routerUsercreated.post('/', create)
-
-export default routerUsercreated
